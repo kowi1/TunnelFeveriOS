d12d487b73bdc48cc884bff1e7d07f5d90663e88
Ayodeji <dolukowi@gmail.com>
2023-10-05 16:01:58 +0100

    Extended bullet physics
    Extended bullet physics

--- cpp/play_scene.cpp
+++ cpp/play_scene.cpp
@@ -164,6 +164,70 @@ PlayScene::PlayScene() : Scene() {
         mUseMove=true;
         mIsmenu=false;}
     
+    
+    
+    
+    
+    dynamicsWorld->setGravity(btVector3(0, -10, 0));
+    
+    {
+            btCollisionShape* groundShape = new btBoxShape(btVector3(btScalar(50.), btScalar(50.), btScalar(50.)));
+
+            collisionShapes.push_back(groundShape);
+
+            btTransform groundTransform;
+            groundTransform.setIdentity();
+            groundTransform.setOrigin(btVector3(0, -56, 0));
+
+            btScalar mass(0.);
+
+            //rigidbody is dynamic if and only if mass is non zero, otherwise static
+            bool isDynamic = (mass != 0.f);
+
+            btVector3 localInertia(0, 0, 0);
+            if (isDynamic)
+                groundShape->calculateLocalInertia(mass, localInertia);
+
+            //using motionstate is optional, it provides interpolation capabilities, and only synchronizes 'active' objects
+            btDefaultMotionState* myMotionState = new btDefaultMotionState(groundTransform);
+            btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, groundShape, localInertia);
+            btRigidBody* body = new btRigidBody(rbInfo);
+
+            //add the body to the dynamics world
+            dynamicsWorld->addRigidBody(body);
+        }
+
+        {
+            //create a dynamic rigidbody
+
+            //btCollisionShape* colShape = new btBoxShape(btVector3(1,1,1));
+            btCollisionShape* colShape = new btSphereShape(btScalar(1.));
+            collisionShapes.push_back(colShape);
+
+            /// Create Dynamic Objects
+            btTransform startTransform;
+            startTransform.setIdentity();
+
+            btScalar mass(1.f);
+
+            //rigidbody is dynamic if and only if mass is non zero, otherwise static
+            bool isDynamic = (mass != 0.f);
+
+            btVector3 localInertia(0, 0, 0);
+            if (isDynamic)
+                colShape->calculateLocalInertia(mass, localInertia);
+
+            startTransform.setOrigin(btVector3(2, 10, 0));
+
+            //using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+            btDefaultMotionState* myMotionState = new btDefaultMotionState(startTransform);
+            btRigidBody::btRigidBodyConstructionInfo rbInfo(mass, myMotionState, colShape, localInertia);
+            btRigidBody* body = new btRigidBody(rbInfo);
+
+            dynamicsWorld->addRigidBody(body);
+        }
+    
+    
 }
 
 
@@ -337,9 +401,73 @@ void PlayScene::OnKillGraphics() {
     CleanUp(&mWallTexture);
     CleanUp(&mLifeGeom);
     hello->~Hello();
+    
+    for (ibullet = dynamicsWorld->getNumCollisionObjects() - 1; ibullet >= 0; ibullet--)
+        {
+            btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[ibullet];
+            btRigidBody* body = btRigidBody::upcast(obj);
+            if (body && body->getMotionState())
+            {
+                delete body->getMotionState();
+            }
+            dynamicsWorld->removeCollisionObject(obj);
+            delete obj;
+        }
+
+        //delete collision shapes
+        for (int j = 0; j < collisionShapes.size(); j++)
+        {
+            btCollisionShape* shape = collisionShapes[j];
+            collisionShapes[j] = 0;
+            delete shape;
+        }
+
+        //delete dynamics world
+        delete dynamicsWorld;
+
+        //delete solver
+        delete solver;
+
+        //delete broadphase
+        delete overlappingPairCache;
+
+        //delete dispatcher
+        delete dispatcher;
+
+        delete collisionConfiguration;
+
+        //next line is optional: it will be cleared by the destructor when the array goes out of scope
+        collisionShapes.clear();
 }
 
 void PlayScene::DoFrame() {
+    
+    
+    
+    for (ibullet = 0; ibullet < 150; ibullet++)
+        {
+            dynamicsWorld->stepSimulation(1.f / 60.f, 10);
+
+            //print positions of all objects
+            for (int j = dynamicsWorld->getNumCollisionObjects() - 1; j >= 0; j--)
+            {
+                btCollisionObject* obj = dynamicsWorld->getCollisionObjectArray()[j];
+                btRigidBody* body = btRigidBody::upcast(obj);
+                btTransform trans;
+                if (body && body->getMotionState())
+                {
+                    body->getMotionState()->getWorldTransform(trans);
+                }
+                else
+                {
+                    trans = obj->getWorldTransform();
+                }
+                //printf("world pos object %d = %f,%f,%f\n", j, float(trans.getOrigin().getX()), float(trans.getOrigin().getY()), float(trans.getOrigin().getZ()));
+                LOGD("world pos object %d = %f,%f,%f\n", j, float(trans.getOrigin().getX()), float(trans.getOrigin().getY()), float(trans.getOrigin().getZ()));
+            }
+        }
+    
+    
     
     float deltaT = mFrameClock.ReadDelta();
     float previousY = mPlayerPos.y;
@@ -433,7 +561,7 @@ void PlayScene::DoFrame() {
     }
 
     // update speed
-    float targetSpeed = PLAYER_SPEED + PLAYER_SPEED_INC_PER_LEVEL * mDifficulty;
+    float targetSpeed = PLAYER_SPEED + PLAYER_SPEED_INC_PER_LEVEL * ((mDifficulty%6)+1);
     float accel = mPlayerSpeed >= 0.0f ? PLAYER_ACCELERATION_POSITIVE_SPEED :
             PLAYER_ACCELERATION_NEGATIVE_SPEED;
    if (mLives <= 0) {
--- cpp/play_scene.hpp
+++ cpp/play_scene.hpp
@@ -26,6 +26,9 @@
 #include "util.hpp"
 #include "HelloWorld.hpp"
 
+#include "btBulletDynamicsCommon.h"
+#include <stdio.h>
+
 class OurShader;
 
 static int life=0;
@@ -296,11 +299,35 @@ class PlayScene : public Scene {
         void UpdateProjectionMatrix();
     private:
     struct android_app* mApp;
-    float animPos=0.0f;
+        float animPos=0.0f;
         float animScale=0.0f;
         int k=1;
         float Pos_step=0.05;
         float Scale_step=0.05;
+    
+    
+    ///-----includes_end-----
+
+        int ibullet;
+        ///-----initialization_start-----
+
+        ///collision configuration contains default setup for memory, collision setup. Advanced users can create their own configuration.
+        btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
+
+        ///use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
+        btCollisionDispatcher* dispatcher = new btCollisionDispatcher(collisionConfiguration);
+
+        ///btDbvtBroadphase is a good general purpose broadphase. You can also try out btAxis3Sweep.
+        btBroadphaseInterface* overlappingPairCache = new btDbvtBroadphase();
+
+        ///the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
+        btSequentialImpulseConstraintSolver* solver = new btSequentialImpulseConstraintSolver;
+
+        btDiscreteDynamicsWorld* dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, overlappingPairCache, solver, collisionConfiguration);
+        btAlignedObjectArray<btCollisionShape*> collisionShapes;
+        
+    
+    
 };
 
 #endif
